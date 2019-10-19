ed6f8d43f79e6a3f4d3d4b09c71aad54f6d10839
Brian Obmalay <brianobmalay@loop-depaulsecure-253-14.depaulsecure-student.depaul.edu>
2019-10-19 15:10:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/studentloans/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.studentloans;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void hey(View v){
+        System.out.print("hey");
+    }
+}
