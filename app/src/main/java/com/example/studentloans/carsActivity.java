d2c92ba18029e724a508c6a813a90ffac58103ad
Maribel Alvarez <Penamaribel005@gmail.com>
2019-10-20 00:47:04 -0500

    Here are cars, pets, phones

--- app/src/main/java/com/example/studentloans/carsActivity.java
+++ app/src/main/java/com/example/studentloans/carsActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class carsActivity extends AppCompatActivity {
 
+    //private String confirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +36,30 @@ public class carsActivity extends AppCompatActivity {
     public void BMW(View v){
         Intent intent = new Intent(this, carsActivity.class);
         startActivity(intent);
+    }
+
+    public static String enterCars(View v) {
+        boolean checked = v.isChecked();
+        String clicked = " ";
+            switch (v.getId()) {
+                case R.id.radioButton12:
+                    if (checked)
+                        clicked = "Honda Civic";
+                    break;
+                case R.id.radioButton13:
+                    if (checked)
+                        clicked = "Chevy Silverado";
+                    break;
+                case R.id.radioButton14:
+                    if (checked)
+                        clicked = "Rolls Royce Ghost";
+                    break;
+                case R.id.radioButton15:
+                    if (checked)
+                        clicked = "BMW 4";
+
+            }
+        return clicked;
+
     }
 }
