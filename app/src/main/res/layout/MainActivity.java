4d5129520d2d24331185ac239d7470cfdeb5a48d
kabaniak <56622816+kabaniak@users.noreply.github.com>
2019-10-19 20:28:29 -0500

    Fix spelling in main

---
+++ app/src/main/res/layout/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.studentloans;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+
+
+import android.os.Bundle;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public void tuition(View v){
+        Intent intent = new Intent(this, TuitionActivity.class);
+        startActivity(intent);
+    }
+
+    public void budget(View v){
+        Intent intent = new Intent(this, budgetActivity.class);
+        startActivity(intent);
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Float> lifestyle = new HashMap<String, Float>();
+
+        // Add keys and values (Food, Costs)
+        lifestyle.put("thrifty_groceries", 176.10f);
+        lifestyle.put("low-cost_groceries", 225.15f);
+        lifestyle.put("moderate_groceries", 279.05f);
+        lifestyle.put("liberal", 348.95f);
+        System.out.println(lifestyle);
+
+        //Add keys and values (Rent, Costs)
+        lifestyle.put("studio", 927.00f);
+        lifestyle.put("1br", 1076.00f);
+        lifestyle.put("2br", 1265.00f);
+        lifestyle.put("3br", 1618.00f);
+        //*add single family??
+
+    }
+}
